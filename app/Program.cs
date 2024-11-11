5fc8142dd99a15358f2448d5e1f3bec9554dfaae
Matheus <matheussolano825@gmail.com>
2024-11-11 20:52:02 -0300

    Erro ao fazer ex-1,aula 2

---
+++ app/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Delaração
+int num;
+string t;
+
+
+//Atribuição
+num = 12;
+//t = "Marco ";
+
+
+int soma = 2 + num;
+Console.WriteLine(soma);
+
+//operadores
+int numero = 2;
+
+float resultado = numero * 6;
+
+resultado = numero / resultado;
+// forma resumida da de cima
+//resultado /= numero;
+
+Console.WriteLine(resultado);
