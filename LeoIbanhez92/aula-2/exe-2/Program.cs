e0762bc11a5322b849fc00346a11c3aa2bdd6ebd
Leonardo Ibanhez <leonardohibanhez@gmail.com>
2024-11-11 22:13:34 -0300

    exercícios da aula 2, coloquei descrição para cada tarefa

---
+++ LeoIbanhez92/aula-2/exe-2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Escreva um programa que some dois números da sua escolha e imprima o resultado
+int resultado = 9 + 85;
+
+Console.WriteLine(resultado);
+
+//Escreva um programa que subtraia dois números da sua escolha e imprima o resultado
+
+int resultado2 = 70 - 35;
+
+Console.WriteLine(resultado2);
+
+//Escreva um programa que some 2 números a sua escolha, armazene e imprima o resultado. Depis, subtraia 5 do resultado e imprima o novo resultado.
+
+int resultado3 = 95 + 75;
+int saldoFinal;
+
+Console.WriteLine(resultado3);
+
+saldoFinal = resultado3 - 5;
+
+Console.WriteLine(saldoFinal);
