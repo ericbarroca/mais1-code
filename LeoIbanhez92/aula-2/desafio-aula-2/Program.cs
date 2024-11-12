fc02474f27b1a864549bcf4c9eb2aeaa487aa70f
Leonardo Ibanhez <leonardohibanhez@gmail.com>
2024-11-12 00:52:42 -0300

    Desafio para receber 2 números. fiz minhas pesquisas para fazer esse programa

---
+++ LeoIbanhez92/aula-2/desafio-aula-2/Program.cs
@@ -0,0 +1,29 @@
+﻿//Escreva um programa que receba 2 números (significa que no prompt serão digitados este números e o programa os armazenará) e imprima o resultado da soma dos mesmos.
+
+Console.WriteLine("Digite um número para somar: ");
+
+string numero1 = Console.ReadLine();
+if (int.TryParse(numero1, out int num1))
+{
+    Console.WriteLine($"O número digitado foi {num1}");
+
+}
+else {
+    Console.WriteLine("A entrada não é um número valido!");
+}
+
+Console.WriteLine("Digite outro número para somar: ");
+
+string numero2 = Console.ReadLine();
+if (int.TryParse(numero2, out int num2)){
+    Console.WriteLine($"O número digitado foi {num2}");
+}
+else {
+    Console.WriteLine("Aentrada não é um número válido");
+}
+
+Console.WriteLine($"Você digitou: {num1} e {num2}!");
+
+int soma = num1 + num2;
+
+Console.WriteLine("O resultado da soma dos dois números que vc digitou foi: " + "" + soma);
