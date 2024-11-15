8ded617aa45e6b3245aade44b6ca62da1272119b
Anaglebia <bia22.sp@icloud.com>
2024-11-14 21:27:36 -0300

    aula 3 funçoes

---
+++ anaGlebia/aula3/aula3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Codigo sem funcao
+
+int num = 3;
+int increment = num + 1;
+
+Console.WriteLine(increment);
+
+// int num2 = 6;
+// int increment = num2 + 2;
+
+// Console.WriteLine(increment);
+
+// Codigo com funcao
+
+void escreveNumMais2(int num){
+    int increment = num + 2;
+    Console.WriteLine(increment);
+}
+
+escreveNumMais2(3);
+escreveNumMais2(6);
