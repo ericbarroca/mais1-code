1a9882564c2d0a56a5c5385b28d18bfd94c294e5
Matheus <matheussolano825@gmail.com>
2024-11-26 12:20:30 -0300

    Adicionando o aula-3/ex-3

--- matheus-586/aula-3/ex-3/Program.cs
+++ matheus-586/aula-3/ex-3/Program.cs
@@ -3,4 +3,25 @@
 //    - Pede para o usuário digitar outro número, o armazena em uma variável.
 //    - Soma os 2 números e imprime o resultado na tela.
 
+int PedeEArmazenaONumeroDigitado(int num1) {
+    
+    Console.WriteLine("Digite o número para ser armazenado: ");
+    
+    while (!int.TryParse(Console.ReadLine(), out num1)) {
+    Console.WriteLine("Insira somente um número inteiro por favor.");
+    }
+    
+    return num1;
+}
 
+int numero1 = PedeEArmazenaONumeroDigitado(0);
+int numero2 = PedeEArmazenaONumeroDigitado(1);
+
+void Soma2NumerosEImprimeNatela(){
+    Console.WriteLine("O Primeiro número digitado foi: " + numero1);
+    Console.WriteLine("O Segundo número digitado foi: " + numero2);
+    
+    Console.WriteLine("A soma dos números é: " + (numero1+numero2));
+}
+
+Soma2NumerosEImprimeNatela();
--- matheus-586/aula-3/ex-3/obj/Debug/net8.0/ex-3.AssemblyInfo.cs
+++ matheus-586/aula-3/ex-3/obj/Debug/net8.0/ex-3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ex-3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+651b4f260ec413761f008fafbb89c25b75993fd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf1ea900df7b1492b30e8afa9441c7e9d431b2c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ex-3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ex-3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
