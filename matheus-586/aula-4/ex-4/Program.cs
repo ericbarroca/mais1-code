b4a69707336acfec5ad0f418a613cb31f53adfa7
Matheus <matheussolano825@gmail.com>
2024-12-12 23:03:44 -0300

    Adicionando a aula-4/ex-4 e aula-4/ex-7

--- matheus-586/aula-4/ex-4/Program.cs
+++ matheus-586/aula-4/ex-4/Program.cs
@@ -1,2 +1,8 @@
 ﻿// - Escreva um programa que leia um número do usuário e exiba sua tabuada de 1 a 10
+Console.WriteLine("Escreva um numero para ver sua tabuada.");
+int numeroEscrito = int.Parse(Console.ReadLine());
 
+for(int j = 2; j <= 10; j++){
+    int tabuada = numeroEscrito * j;
+    Console.WriteLine (tabuada);
+}
--- matheus-586/aula-4/ex-7/Program.cs
+++ matheus-586/aula-4/ex-7/Program.cs
@@ -1,1 +1,14 @@
 ﻿// - Escreva um programa que leia três notas de um aluno, calcule a média e exiba uma mensagem indicando se ele foi aprovado (média ≥ 7), está em recuperação (5 ≤ média < 7) ou foi reprovado (média < 5). Usar funções.
+Console.WriteLine("escreva a primeira nota do aluno: ");
+int nota1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("escreva a segunda nota do aluno: ");
+int nota2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("escreva a terceira nota do aluno: ");
+int nota3 = int.Parse(Console.ReadLine());
+
+int somatorio = nota1 + nota2 + nota3;
+int media = somatorio/3;
+
+Console.WriteLine("Sua média é: " + media);
