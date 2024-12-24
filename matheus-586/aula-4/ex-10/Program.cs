b85c378ae46f5fd3d88feee520b2fc20adb3ece7
Matheus <matheussolano825@gmail.com>
2024-12-23 22:09:50 -0300

    Codigo matheus-586/aula-4/ex-8 Finalizado

--- matheus-586/aula-4/ex-10/Program.cs
+++ matheus-586/aula-4/ex-10/Program.cs
@@ -1,1 +1,18 @@
 ﻿// - Escreva um programa que ordene 10 números digitados pelo usuário e imprima-os ordenadamente em uma linha.
+
+// Declarar vetor
+int[] numeros = new int[10];
+
+// Ler números
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Digite o número {i + 1}: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+// Ordenar números
+Array.Sort(numeros);
+
+// Imprimir números ordenados
+Console.WriteLine("Números ordenados:");
+Console.WriteLine(string.Join($" ", numeros));
--- matheus-586/aula-4/ex-8/Program.cs
+++ matheus-586/aula-4/ex-8/Program.cs
@@ -1,2 +1,32 @@
 ﻿// - Escreva um programa que crie uma matriz de identidade 3x3. Uma matriz de identidade é uma matriz quadrada em que todos os elementos da diagonal principal são 1, e todos os outros elementos são 0. Exiba a matriz resultante.
 
+// Declarar matriz 
+int[,] matrizIdentidade = new int[3, 3];
+
+for (int linha = 0; linha < matrizIdentidade.GetLength(0); linha++)
+{
+    for (int coluna = 0; coluna < matrizIdentidade.GetLength(1); coluna++)
+    {
+        if (linha == coluna)
+        {
+            matrizIdentidade[linha, coluna] = 1;
+        }
+    }
+}
+
+
+// Imprimir matriz
+void imprimirMatriz(int[,] matriz)
+{
+    for (int linha = 0; linha < matrizIdentidade.GetLength(0); linha++)
+    {
+        for (int coluna = 0; coluna < matrizIdentidade.GetLength(1); coluna++)
+        {
+            Console.Write($"{matrizIdentidade[linha, coluna]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Matriz de Identidade 3x3:");
+imprimirMatriz(matrizIdentidade);
