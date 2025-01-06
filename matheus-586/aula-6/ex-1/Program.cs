6688ebc674533b0a0767ba5446ccaae23474fb92
Matheus <matheussolano825@gmail.com>
2025-01-06 20:39:18 -0300

    Finalizado o ex-1 e o ex-3 da aula 6

--- matheus-586/aula-6/ex-1/Program.cs
+++ matheus-586/aula-6/ex-1/Program.cs
@@ -1,2 +1,13 @@
 ﻿// - Praticar a criação de classes e instâncias de objetos. Descrição: Crie uma classe chamada `Carro` com os seguintes atributos: `Marca`, `Modelo` e `Ano`. Adicione um método chamado `ExibirDetalhes` que exiba os detalhes do carro. Em seguida, crie uma instância da classe `Carro`, atribua valores aos atributos e chame o método para exibir os detalhes.
 
+public class Carro {
+    // Atributos
+    public string Marca;
+    public string Modelo;
+    public int Ano;
+    
+    // Métodos
+    public void ExibirDetalhes() {
+        Console.WriteLine("Você tem um carro da " + $"Marca: {Marca}, Modelo: {Modelo} e Ano: {Ano}");
+    }
+}
