2623a63afbf5284e327177fb2393a9b5f5de3dfa
Matheus <matheussolano825@gmail.com>
2025-01-10 19:48:09 -0300

    Alterei o nome da pasta desafio-aula-3, para desafios e fiz o aula-4/desafios/desafio-1

--- matheus-586/aula-4/desafios/desafio-1/Program.cs
+++ matheus-586/aula-4/desafios/desafio-1/Program.cs
@@ -1,1 +1,34 @@
 ﻿// - Escreva um programa que simule um sistema de login. O usuário deve digitar um nome de usuário e senha. Se o nome de usuário for "admin" e a senha for "1234", exiba uma mensagem de "Login bem-sucedido", caso contrário, peça para ele tentar novamente. O sistema deve permitir apenas três tentativas.
+
+int tentativas = 0;
+const string usuarioValido = "admin";
+const string senhaValida = "1234";
+bool loginEfetuado = false;
+
+while (tentativas < 3 && !loginEfetuado)
+{
+    Console.Write("Digite o nome de usuário: ");
+    string usuario = Console.ReadLine();
+
+    Console.Write("Digite a senha: ");
+    string senha = Console.ReadLine();
+
+    if (usuario == usuarioValido && senha == senhaValida)
+    {
+        Console.WriteLine("Login bem-sucedido!");
+        loginEfetuado = true;
+    }
+    else
+    {
+        tentativas++;
+        Console.WriteLine($"Tentativa {tentativas}/3. Usuário ou senha inválidos. Tente novamente.");
+    }
+}
+
+if (!loginEfetuado)
+{
+    Console.WriteLine("Número máximo de tentativas alcançado.");
+}
+
+Console.ReadKey();
+
