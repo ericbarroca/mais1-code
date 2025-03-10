95dbef24917b2d5a6afc866e5bab98c968e5d617
Matheus <matheussolano825@gmail.com>
2025-03-10 19:56:38 -0300

    .

--- matheus-586/aula-6/ex-1/criandoCarro.cs
+++
@@ -1,8 +0,0 @@
-//Em seguida, crie uma instância da classe `Carro`, atribua valores aos atributos e chame o método para exibir os detalhes.
-
-Carro carro1 = new Carro();
-carro1.Marca = "Nissan";
-carro1.Modelo = "S MT";
-carro1.Ano = 2019;
-
-carro1.ExibirDetalhes();
