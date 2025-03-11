dd84d933a623c1fa6d7c1171cc2cf1fb1d53bfa4
Matheus <matheussolano825@gmail.com>
2025-03-10 21:44:00 -0300

    Organizando os exercicios

---
+++ matheus-586/aula-6/ex-1/Program.cs
@@ -0,0 +1,1 @@
+﻿// - Praticar a criação de classes e instâncias de objetos. Descrição: Crie uma classe chamada `Carro` com os seguintes atributos: `Marca`, `Modelo` e `Ano`. Adicione um método chamado `ExibirDetalhes` que exiba os detalhes do carro. Em seguida, crie uma instância da classe `Carro`, atribua valores aos atributos e chame o método para exibir os detalhes.
