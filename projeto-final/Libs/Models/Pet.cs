513b5f172345dc7b3080e4bcd98829259d230131
Matheus <matheussolano825@gmail.com>
2025-03-18 13:03:30 -0300

    Fiz as alterações no pet models

--- projeto-final/Libs/Models/Pet.cs
+++ projeto-final/Libs/Models/Pet.cs
@@ -1,29 +1,35 @@
+using Libs.Repository;
+
 namespace Libs.Models;
 
-public enum Especie {
+public enum Especie
+{
     cachorro,
     gato
 }
 
-public class Pet {
-    public int ID {get; set;}
+public class Pet
+{
+    public int ID { get; set; }
 
-    public string Nome {get;set;}
+    public string Nome { get; set; }
 
-    public DateTime DataNascimento {get;set;}
+    public DateTime DataNascimento { get; set; }
 
-    public Especie Especie {get;set;}
+    public Especie Especie { get; set; }
 
-    public string Raca {get;set;}
+    public string Raca { get; set; }
 
-    public Documento TutorID {get;set;}
+    public Documento TutorID { get; set; }
 
-    public Pet() {
-        
+    public Pet()
+    {
+
     }
 
     public Pet(string nome, DateTime dataNascimento,
-    Especie especie, string raca) {
+    Especie especie, string raca)
+    {
 
         this.Nome = nome;
         this.DataNascimento = dataNascimento;
@@ -31,9 +37,22 @@ public class Pet {
         this.Raca = raca;
     }
 
-    public Pet(DAO.Pet pet): this(pet.Nome,
-    pet.DataNascimento, (Especie)pet.Especie, pet.Raca) {
+    public Pet(DAO.Pet pet) : this(pet.Nome,
+    pet.DataNascimento, (Especie)pet.Especie, pet.Raca)
+    {
         this.ID = pet.ID;
         this.TutorID = pet.TutorID;
+    }
+
+    public List<Vacina> Pets(VacinaRepository vacinaRepository)
+    {
+        List<Vacina> vacinas = vacinaRepository.List();
+        return vacinas.FindAll(t => t.PetID == ID).ToList();
+    }
+
+    public bool UpsertPet(VacinaRepository vacinaRepository, Vacina vacina)
+    {
+        vacina.PetID = ID;
+        return vacinaRepository.Upsert(vacina);
     }
 }
