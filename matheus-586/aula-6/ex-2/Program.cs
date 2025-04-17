add1e594c5ef5937491e3dfb0b66414681d5a049
Matheus <matheussolano825@gmail.com>
2025-04-17 15:17:46 -0300

    finalizando os exercicios da aula-6

--- matheus-586/aula-6/ex-2/Program.cs
+++ matheus-586/aula-6/ex-2/Program.cs
@@ -1,1 +1,34 @@
 ﻿// - Crie uma classe chamada `ContaBancaria` com os atributos privados `numeroConta` e `saldo`. Adicione métodos públicos `Depositar` e `Sacar` para modificar o saldo, e métodos `GetNumeroConta` e `GetSaldo` para acessar os atributos. Teste a classe criando uma instância e realizando algumas operações.
+
+public class ContaBancaria{
+    private int numeroConta;
+    private int saldo;
+
+    public void SetSaldo(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public void GetSaldo() {
+       Console.WriteLine("Saldo da Conta: " + saldo);
+    }
+
+    public void SetNumeroConta(int numeroConta){
+        this.numeroConta = numeroConta;  
+    }
+    public void GetNumeroConta() {
+        Console.WriteLine("Numero da conta: " + numeroConta);
+    }
+    
+    public void Depositar(int depositar) {
+        this.saldo += depositar;
+        Console.WriteLine("Saldo atual (pós-depósito): " + saldo);
+    }
+
+    public void Sacar(int sacar) {
+        this.saldo -= sacar;
+        Console.WriteLine("Saldo atual (pós-sacar): " + saldo);
+    }
+}
+
+
+
--- matheus-586/aula-6/ex-4/Program.cs
+++ matheus-586/aula-6/ex-4/Program.cs
@@ -1,1 +1,37 @@
 ﻿// - Crie uma classe abstrata chamada `FormaGeometrica` com um método abstrato `CalcularArea`. Em seguida, crie duas subclasses `Retangulo` e `Circulo`, cada uma implementando o método `CalcularArea`. Teste as classes criando instâncias de `Retangulo` e `Circulo`, e chamando o método `CalcularArea`.
+
+
+abstract class FormaGeometrica
+{
+
+  public abstract void CalcularArea(int b, int altura);
+
+}
+
+class Retangulo : FormaGeometrica
+{
+  public override void CalcularArea(int B, int H)
+  {
+    Console.WriteLine("A Área desse retangulo é: " + (B*H));
+  }
+}
+
+class Circulo : FormaGeometrica
+{
+  public override void CalcularArea(int Pi, int Raio)
+  {
+    Console.WriteLine("A Área desse circulo é: " + (Pi*(Raio*Raio)));
+  }
+}
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    Retangulo retangulo = new Retangulo();
+    retangulo.CalcularArea( 5,  6);
+    
+    Circulo circulo = new Circulo();
+    circulo.CalcularArea( (22/7),  6);
+  }
+}
