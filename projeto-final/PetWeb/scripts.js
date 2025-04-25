0cbb19d356587144e4fa1e3c4f880ceb12b70320
Eric Barroca <barrocaeric@gmail.com>
2025-04-24 21:21:30 -0300

    Form novo pet

--- projeto-final/PetWeb/scripts.js
+++ projeto-final/PetWeb/scripts.js
@@ -2,9 +2,23 @@ const baseUrl = "http://localhost:5118";
 const getTutorEndpoint = "tutor";
 const getPetsEndpoint = "tutor/{id}/pets";
 
-const petItem = '<button key="{id}" type="button" class="list-group-item list-group-item-action " aria-current="true" data-bs-toggle="list" >{name}</button>';
+const petItem = '<button id="pet-{id}" key="{id}" type="button" class="list-group-item list-group-item-action" aria-current="true" data-bs-toggle="list">{name}</button>';
 
 (async () => {
+
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
 
     const notification = document.getElementById('liveToast')
     const toast = new bootstrap.Toast(notification)
@@ -14,6 +28,17 @@ const petItem = '<button key="{id}" type="button" class="list-group-item list-gr
 
     if(tutor) {
         pets = await renderizaPet(notification, tutor)
+
+        const btnNewPet = document.getElementById('btnNewPet')
+        btnNewPet.addEventListener('click', (e)=> {
+            const frmNewPet = document.getElementById('frmNewPet')
+            frmNewPet.hidden = false
+
+            const petList = document.getElementById("petList")
+            Array.from(petList.children).forEach(i=>{
+                i.className = 'list-group-item list-group-item-action'
+            })
+        })
     }
 
 
@@ -27,6 +52,9 @@ async function loadTutor(notification) {
     if (error) {
         return false
     }
+
+    const userName = document.getElementById("userName")
+    userName.textContent = tutor.nome
 
     return tutor
 }
@@ -43,7 +71,7 @@ async function renderizaPet(notification, tutor) {
     const petList = document.getElementById("petList")
 
     Array.from(pets).forEach(pet => {
-        const el = petItem.replace("{name}", pet.nome).replace("{id}", pet.id);
+        const el = petItem.replace("{name}", pet.nome).replaceAll("{id}", pet.id);
         const doc = parser.parseFromString(el, 'text/html');
         petList.appendChild(doc.body.firstChild)
     })
@@ -61,9 +89,6 @@ function hasError(notification, data) {
         toastTitle.textContent = "Erro"
         toastBody.textContent = data.error
         toast.show()
-    } else {
-        usernameElement = document.getElementById('userName')
-        usernameElement.textContent = data.nome
     }
 }
 
